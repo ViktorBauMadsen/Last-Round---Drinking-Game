18a1697c6656ecd1e17812656d64b109fd42cb54
Spidaus <william.spittau@gmail.com>
2025-05-01 10:43:02 +0200

    RandomScrollManager

---
+++ Assets/Scripts/ScrollScene/RandomScrollAnimator.cs
@@ -0,0 +1,82 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RandomScrollAnimator : MonoBehaviour
+{
+    public List<GameObject> scrolls; // Assign 4 scrolls here (not the final one)
+    public GameObject finalScroll;   // Assign the last scroll here
+    public float scrollDuration = 3f;
+
+    private static List<int> shownIndexes = new List<int>();
+    private static int totalScrollsShown = 0;
+
+    private void Start()
+    {
+        ShowScroll();
+    }
+
+    void ShowScroll()
+    {
+        totalScrollsShown++;
+
+        GameObject scrollToShow = null;
+
+        if (totalScrollsShown < 5)
+        {
+            // Pick a random scroll that hasn't been shown yet
+            int index;
+            do
+            {
+                index = Random.Range(0, scrolls.Count);
+            } while (shownIndexes.Contains(index));
+
+            shownIndexes.Add(index);
+            scrollToShow = Instantiate(scrolls[index], transform);
+        }
+        else
+        {
+            // Show the final scroll
+            scrollToShow = Instantiate(finalScroll, transform);
+        }
+
+        // Animate scroll from left to center using coroutine
+        RectTransform rt = scrollToShow.GetComponent<RectTransform>();
+        rt.anchoredPosition = new Vector2(-Screen.width, 0); // Start off-screen
+
+        StartCoroutine(AnimateScroll(rt, scrollDuration));
+    }
+
+    IEnumerator AnimateScroll(RectTransform rt, float duration)
+    {
+        Vector2 startPos = rt.anchoredPosition;
+        Vector2 endPos = Vector2.zero;
+        float elapsed = 0f;
+
+        while (elapsed < duration)
+        {
+            rt.anchoredPosition = Vector2.Lerp(startPos, endPos, elapsed / duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        rt.anchoredPosition = endPos;
+
+        OnScrollComplete();
+    }
+
+    void OnScrollComplete()
+    {
+        if (totalScrollsShown < 5)
+        {
+            SceneManager.LoadScene("SelectBeerScene");
+        }
+        else
+        {
+            SceneManager.LoadScene("HomeScreen");
+            shownIndexes.Clear();
+            totalScrollsShown = 0;
+        }
+    }
+}
