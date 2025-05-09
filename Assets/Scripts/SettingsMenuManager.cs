d4abaceacda0b0f5a2cb590c261d8583558c4602
Rasmus <rasmushovmand12@gmail.com>
2025-05-09 12:44:00 +0200

    Settings page
    Settings page set up
    + Graphics settings

---
+++ Assets/Scripts/SettingsMenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SettingsMenuManager : MonoBehaviour
+{
+    public TMP_Dropdown graphicsDropdown;
+
+
+    public void ChangeGraphicsQuality()
+    {
+        QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
